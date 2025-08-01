d6cad81565a4887ac2008820270462df0b841c45
FatihZee <viananto1234@gmail.com>
2025-08-01 10:35:26 +0700

    feat: Implement urgency analysis for chat messages and update Firebase integration

--- Backend/firebase.js
+++ Backend/firebase.js
@@ -1,15 +1,11 @@
-// firebase.js
-const { initializeApp, applicationDefault } = require('firebase-admin/app');
-const { getFirestore } = require('firebase-admin/firestore');
 const admin = require('firebase-admin');
-
-// Ganti dengan file service account kamu atau gunakan env var
-const serviceAccount = require('./chat-ai-agent-76597-firebase-adminsdk-fbsvc-b0932fddee.json');
+const serviceAccount = require('./chat-ai-agent-76597-2e885bc11736.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
-const db = getFirestore();
+const db = admin.firestore();
+db.settings({ ignoreUndefinedProperties: true });
 
 module.exports = db;
--- Backend/routes/chatRoutes.js
+++ Backend/routes/chatRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const authenticate = require("../middlewares/authMiddleware");
+const authenticate = require('../middlewares/authMiddleware');
+const authorizeRole = require('../middlewares/roleMiddleware');
 const chatController = require('../controllers/chatController');
 
 router.post('/', authenticate, chatController.sendChat);
 router.get('/', authenticate, chatController.getChats);
+router.get('/urgent', authenticate, authorizeRole('admin'), chatController.getUrgentChats);
 
 module.exports = router;
